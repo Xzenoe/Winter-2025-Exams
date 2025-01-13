8e50fe071608836fbb9456ba3619f53dd7e1e2d1
Xzenoe <bogdantepajkin@gmail.com>
2025-01-13 21:20:51 +0200

    rewrote code for readability

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -2,9 +2,11 @@
 // Reverse an array, you can't use .reverse()
 'use strict';
 const invert = (array) => {
-  const result = Object.keys(array);
-  result.forEach((i) => (result[i] = array.pop()));
+  const result = [];
+  const length = array.length;
+  for (let i = 0; i < length; i++) {
+    result.push(array.pop());
+  }
   return result;
 };
-
 module.exports = invert;
