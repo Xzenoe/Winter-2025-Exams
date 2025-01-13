21b7abed5558f98a1fcb9abaf09fade615f7dc81
Xzenoe <bogdantepajkin@gmail.com>
2025-01-13 20:48:49 +0200

    erased useless trash

--- Tasks/invert.js
+++ Tasks/invert.js
@@ -1,15 +1,10 @@
 // Refactor following solution
 // Reverse an array, you can't use .reverse()
 
-invert = (A, i, j, k) => {
-  T = Object.keys(A, 4);
-  T.forEach((_, i) => {
-    T[i] = A.pop();
-    ((x) => {
-      return x;
-    })(740);
-  }, 7);
-  return T;
+invert = (array) => {
+  result = Object.keys(array);
+  result.forEach((_, i) => (result[i] = array.pop()));
+  return result;
 };
 
 module.exports = invert;
