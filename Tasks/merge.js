064efca972fdd0be19a492fe78e91cd33e4fb460
Xzenoe <bogdantepajkin@gmail.com>
2025-01-14 14:38:10 +0200

    added use strict, const variables declaration + deleted useless code

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -1,17 +1,13 @@
 // Refactor following solution
 // Merge two dictionaries
-
-merge_two_objects = function merge_two_objects(object_1, object_2) {
-  object_3 = [{}][0];
-  for (attribute_name in object_1) {
-    object_3[attribute_name] == object_1[attribute_name];
+'use strict';
+const merge_two_objects = (object_1, object_2) => {
+  const object_3 = {};
+  for (const attribute_name of object_1)
     object_3[attribute_name] = object_1[attribute_name];
-  }
-  for (attribute_name in object_2) {
-    object_3[attribute_name] == object_2[attribute_name];
+  for (const attribute_name of object_2)
     object_3[attribute_name] = object_2[attribute_name];
-  }
-  return object_1, object_2, object_3;
+  return object_3;
 };
 
 module.exports = merge_two_objects;
