1e03555e3082160881fe6e3f6e795cd7d2541933
Xzenoe <bogdantepajkin@gmail.com>
2025-01-16 19:41:51 +0200

    Update sample.js

--- Tasks/sample.js
+++ Tasks/sample.js
@@ -1,10 +1,10 @@
 // Refactor following solution
 // Get one random element from an array
 'use strict';
-const SAMPLE = (arr) => {
+const sample = (arr) => {
   let randomIndex = Math.floor(Math.random() * arr.length);
   const result = arr[randomIndex];
   return result;
 };
 
-module.exports = SAMPLE;
+module.exports = sample;
